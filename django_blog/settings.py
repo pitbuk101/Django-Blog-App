2ca8655f79bef6c7a245c73c19d72d1ea4a60165
Prateek Tiwari <nopanictri@yahoo.com>
2023-05-29 21:35:59 +0530

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '88-met%yvxkqz&=)r3e3%z6afahh(q+3f-kueoho@)owc0#mo3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
